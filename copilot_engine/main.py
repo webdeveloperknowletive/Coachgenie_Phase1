ba671bd96d95e254f7e10e457f7e28b764a11e67
Geeky_Lokesh_2890 <lokeshsohanda27@gmail.com>
2026-06-15 15:40:42 +0530

    Add allowed origin for CORS middleware

--- copilot_engine/main.py
+++ copilot_engine/main.py
@@ -33,6 +33,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://coachgenie-phase1.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
